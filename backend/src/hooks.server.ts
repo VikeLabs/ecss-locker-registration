1a5e960a7d969e62aa99aa487f3dc72cf190c684
Malcolm Seyd <malcolm@seyd.ca>
2023-05-08 23:24:36 -0700

    improve auth hooks
    - for admin panel, direct users to DirIT
    - for user pages, redirect to login

--- backend/src/hooks.server.ts
+++ backend/src/hooks.server.ts
@@ -1,10 +1,19 @@
 import { base } from '$app/paths';
 import { text, type Handle } from '@sveltejs/kit';
-import { authorize } from './routes/admin/auth.server';
+import { authorize as adminAuthorize } from './routes/admin/auth.server';
+import { authorize as userAuthorize } from '$lib/auth.server';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	if (event.url.pathname.startsWith(`${base}/admin`) && !authorize(event.cookies).authorized) {
-		return text('Sorry bud, no can do!', { status: 401 });
+	if (event.url.pathname.startsWith(`${base}/admin`) && !adminAuthorize(event.cookies).authorized) {
+		return text('Please contact the Director of IT to gain access.', { status: 401 });
+	} else if (
+		event.url.pathname.startsWith(`${base}/lockers`) &&
+		!userAuthorize(event.cookies).authorized
+	) {
+		return new Response("You're not logged in", {
+			status: 301,
+			headers: { location: `${base}/login` }
+		});
 	}
 	return resolve(event);
 };
