b610c69f7cbe341d8aaf9a635be85c4b9cdb03d1
Malcolm Seyd <malcolm@seyd.ca>
2023-05-09 00:09:46 -0700

    stub email functions

---
+++ backend/src/lib/email.ts
@@ -0,0 +1,15 @@
+export function sendRegisterEmail(email: string, locker: string) {
+	console.log(`TODO register locker ${locker} to ${email}`);
+}
+
+export function sendLoginEmail(email: string) {
+	console.log(`TODO login email to ${email}`);
+}
+
+export function sendTransferEmail(email: string, locker: string) {
+	console.log(`TODO transfer locker ${locker} to ${email}`);
+}
+
+export function sendExpiryEmail(email: string, locker: string) {
+	console.log(`TODO expiry reminder about locker ${locker} to ${email}`);
+}
