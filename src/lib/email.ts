93a7a2b7600f82a23134b39c8df341092909fdf9
Malcolm Seyd <malcolm@seyd.ca>
2023-05-22 01:46:49 -0700

    email sending + switch to node

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,5 +1,15 @@
 import { base } from "$app/paths";
 import { makeMagicToken } from "./magic";
+import { env } from "$env/dynamic/private";
+import { SMTPClient } from "emailjs";
+
+const client = new SMTPClient({
+  host: "smtp.gmail.com",
+  port: 465,
+  ssl: true,
+  user: env.GMAIL_USER,
+  password: env.GMAIL_PASSWORD,
+});
 
 let prefix = "";
 // called from src/hooks.server.ts on every request (hopefully)
@@ -20,39 +30,55 @@ export async function sendRegisterEmail(
   locker: string,
   name: string
 ) {
-  console.log(`TODO register locker ${locker} to ${email}`);
   const token = await makeMagicToken({
     type: "register",
     user: email,
     locker,
     name,
   });
-  const msg = `Click here to register locker ${locker}: ${makeMagicLink(
+  const text = `Click here to register locker ${locker} for ${email}: ${makeMagicLink(
     token
   )}`;
-  console.log(msg);
+  await client.sendAsync({
+    from: `Lockers <${env.GMAIL_USER}>`,
+    to: email,
+    subject: `ESS Locker Registration: ${locker}`,
+    text,
+  });
 }
 
 export async function sendLoginEmail(email: string) {
-  console.log(`TODO login email to ${email}`);
   const token = await makeMagicToken({ type: "login", user: email });
-  const msg = `Click here to login: ${makeMagicLink(token)}`;
-  console.log(msg);
+  const text = `Click here to login as ${email}: ${makeMagicLink(token)}`;
+  await client.sendAsync({
+    from: `Lockers <${env.GMAIL_USER}>`,
+    to: email,
+    subject: `ESS Locker Login`,
+    text,
+  });
 }
 
 export async function sendTransferEmail(email: string, locker: string) {
-  console.log(`TODO transfer locker ${locker} to ${email}`);
   const token = await makeMagicToken({ type: "transfer", user: email, locker });
-  const msg = `You have been sent a locker transfer request for locker ${locker}. Click here to accept: ${makeMagicLink(
+  const text = `You have been sent a locker transfer request for locker ${locker}. Click here to accept: ${makeMagicLink(
     token
   )}`;
-  console.log(msg);
+  await client.sendAsync({
+    from: `Lockers <${env.GMAIL_USER}>`,
+    to: email,
+    subject: `ESS Locker Transfer Request: ${locker}`,
+    text,
+  });
 }
 
 export async function sendExpiryEmail(email: string, locker: string) {
-  console.log(`TODO expiry reminder about locker ${locker} to ${email}`);
-  const msg = `Your locker ${locker} is about to expire. Renew it at ${makeLink(
+  const msg = `Your locker ${locker} has expired. Renew it at ${makeLink(
     `lockers/${locker}`
   )}`;
-  console.log(msg);
+  await client.sendAsync({
+    from: `Lockers <${env.GMAIL_USER}>`,
+    to: email,
+    subject: `ESS Locker Expiry: ${locker}`,
+    text: msg,
+  });
 }
