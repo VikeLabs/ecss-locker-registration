a475d484e234e9fd96bb4bcec2eec4f661373117
Malcolm Seyd <malcolm@seyd.ca>
2023-05-24 12:12:32 -0700

    make cute error page

---
+++ src/routes/(app)/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
+
+<main>
+  <h1>
+    An
+    <span class="underline decoration-wavy decoration-red-500">error</span>
+    occurred
+  </h1>
+  <p>{$page.error?.message}</p>
+</main>
